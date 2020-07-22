57151923321c97bddf3d3f57126d3ca18305d9a9
Susanna <sfalan@utu.fi>
2020-07-22 16:12:17 +0300

    filter events per month

--- src/components/EventsContainer.js
+++ src/components/EventsContainer.js
@@ -6,28 +6,42 @@ import lyytiApi from '../service.js'
 
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+const filterEvents = (events, selectedTime) => 
+    events.filter(event => 
+        new Date(event.start_time * 1000).getMonth() === selectedTime.month 
+        && new Date(event.start_time * 1000).getFullYear() === selectedTime.year)
+
 function EventsContainer() {
-    const [currentMonth, setCurrentMonth] = useState();
+    const [currentTime, setCurrentTime] = useState();
+    const [selectedTime, setSelectedTime] = useState();
     const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([])
 
     useEffect(() => {
-        const currentMonth = new Date()
-        setCurrentMonth(currentMonth.getMonth())
-        lyytiApi("events?as_array=1").then(res => setEvents(res.results))
+        const currentTime = new Date()
+        setCurrentTime(currentTime)
+        setSelectedTime({ month: currentTime.getMonth(), year: currentTime.getFullYear() })
+
+        lyytiApi("events?as_array=1")
+        .then(res => setEvents(res.results))
     }, [])
+
+    useEffect(() => {
+        setFilteredEvents(filterEvents(events, selectedTime))
+    }, [selectedTime, events])
 
   return (
     <div className="eventListContainer">
         <div className="oneLine">
             <div className="monthSelector">
                 <ChevronLeft/>
-                <h2>{monthNames[currentMonth]}</h2>
-                <ChevronRight/>
+                {selectedTime && <h2>{monthNames[selectedTime.month] + ' ' + selectedTime.year}</h2>}
+                <ChevronRight />
             </div>
             <Sort />
         </div>
         
-        {events.map(event => {
+        {filteredEvents.map(event => {
             return <Event key={event.id} event={event}/>
         })}
     </div>
