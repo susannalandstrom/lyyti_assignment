95e28f9f8ca5bfa49f9cb7e71f8f029689b87bdc
Susanna <sfalan@utu.fi>
2020-07-22 22:53:21 +0300

    add badge to display num of favorites

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../App.css';
-import { Typography } from '@material-ui/core';
+import { Typography, Badge } from '@material-ui/core';
 import { Favorite } from '@material-ui/icons';
 
-function Header() {
+function Header(props) {
 
   return (
-    <div className="oneLine" style={{ height: '100px' }}>
-        <Typography>Paperstr Events</Typography>
-        <Favorite />
+    <div className="oneLine" style={{ height: '100px', padding: '0 200px' }}>
+        <Typography variant="h1">Paperstr Events</Typography>
+        <Badge badgeContent={props.favorites.length} color="secondary">
+            <Favorite fontSize="large"/>
+        </Badge>
     </div>
   );
 }
