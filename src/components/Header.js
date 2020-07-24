5060f9dc8f70909c0bff34815a044343091b6af8
Susanna <sfalan@utu.fi>
2020-07-24 12:19:47 +0300

    update to use FavoritesPopover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import '../App.css';
-import { Typography, Badge, Tooltip, IconButton } from '@material-ui/core';
-import { Favorite } from '@material-ui/icons';
+import { Typography } from '@material-ui/core';
+import FavoritesPopover from './FavoritesPopover'
 
 function Header(props) {
 
   return (
     <div className="header">
         <Typography variant="h1">Paperstr Events</Typography>
-        <Tooltip title="Favorites">
-            <IconButton>
-                <Badge badgeContent={props.favorites.length} color="secondary">
-                    <Favorite fontSize="large"/>
-                </Badge>
-            </IconButton>
-        </Tooltip>
+        <FavoritesPopover favorites={props.favorites}/>
     </div>
   );
 }
