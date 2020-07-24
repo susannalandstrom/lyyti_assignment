74fde5d9a645896021de374badc8aa9f46a15574
Susanna <sfalan@utu.fi>
2020-07-24 12:21:25 +0300

    update to use imported functions

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,52 +2,29 @@ import React from 'react';
 import '../App.css';
 import { Typography, Tooltip, IconButton } from '@material-ui/core';
 import { Favorite, FavoriteBorder } from '@material-ui/icons';
-
-const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+import { getName } from '../functions/getName'
+import { getCategories } from '../functions/getCategories'
+import { formatTime } from '../functions/formatTime'
 
 function Event(props) {
-
-  const getName = () => {
-    const keys = Object.keys(props.event.name)
-    if (keys.includes('en')) {
-      return props.event.name.en
-    }
-    else return props.event.name[keys[0]]
-  }
-
-  const formatTime = (startTime) => {
-    const minute = startTime.getMinutes()
-    const second = startTime.getSeconds()
-    const minuteFormatted = minute < 10 ? "0" + minute : minute
-    const secondFormatted = second < 10 ? "0" + second : second
-    
-    return weekdays[startTime.getDay()] + ' ' 
-            + startTime.getDate() + '.' 
-            + startTime.getMonth() + 1 + '.' 
-            + startTime.getFullYear() + ' ' 
-            + startTime.getHours() + ':' 
-            + minuteFormatted + ':' 
-            + secondFormatted
-  }
 
   return (
     <div className="eventContainer">
       <div className="oneLine">
-        <Typography style={{ fontSize: '18px', fontWeight: 'bold', marginBottom: '10px' }}>{getName()}</Typography>
+        <Typography variant="h3">{getName(props.event)}</Typography>
         {props.isFavorite 
         ? <IconButton>
-              <Favorite /> 
+              <Favorite color="primary"/> 
           </IconButton>
         : <Tooltip title="Add to favorites" placement="left">
               <IconButton onClick={(e) => props.setAsFavorite(e, props.event)}>
-                  <FavoriteBorder/>
+                  <FavoriteBorder color="primary"/>
               </IconButton>
           </Tooltip>}
       </div>
-      
       <div className="oneLine">
-        <Typography className="category">{props.event.category[1].title}</Typography>
-        <Typography>{formatTime(new Date(props.event.start_time * 1000))}</Typography>
+        {getCategories(props.event).map(category => <Typography className="category">{category}</Typography>)}
+        <Typography>{formatTime(props.event.start_time)}</Typography>
       </div>
     </div>
   );
