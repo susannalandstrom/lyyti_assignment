3daae27415808ec556f72f729bc0b5873e5ddcce
Susanna <sfalan@utu.fi>
2020-07-24 12:19:16 +0300

    add component to display favorites

---
+++ src/components/FavoritesPopover.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import '../App.css';
+import { IconButton, Tooltip, Badge, Popover, Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Favorite, Clear } from '@material-ui/icons';
+import { getName } from '../functions/getName';
+import { getCategories } from '../functions/getCategories'
+import { formatTime } from '../functions/formatTime'
+
+function FavoritesPopover(props) {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const id = open ? 'favorites-popover' : undefined;
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Tooltip title="Favorites" placement="left">
+                <IconButton aria-describedby={id} onClick={handleClick}>
+                    <Badge badgeContent={props.favorites.length} color="secondary">
+                        <Favorite fontSize="large"/>
+                    </Badge>
+                </IconButton>
+            </Tooltip>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}>
+                {props.favorites.map(favorite => {
+                    return (
+                        <Card>
+                            <CardHeader 
+                                variant="h3" 
+                                title={getName(favorite)} 
+                                action={
+                                    <IconButton>
+                                        <Clear fontSize="small" color="primary"/>
+                                    </IconButton>
+                                }
+                            />
+                            <CardContent>
+                                {getCategories(favorite).map(category => <Typography color="secondary">{category}</Typography>)}
+                                <Typography>{formatTime(favorite.start_time)}</Typography>
+                            </CardContent>
+                        </Card>
+                    )
+                })}
+            </Popover>
+        </div>
+    );
+}
+
+export default FavoritesPopover;
