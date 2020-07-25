566cb40d86005f70d4c6732656dfdf57f52f4524
Susanna <sfalan@utu.fi>
2020-07-25 21:08:19 +0300

    update routes and history

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import Header from './components/Header'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import theme from './MuiTheme'
 import {
-  BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  useHistory,
+  useLocation
 } from "react-router-dom";
+import EventDetailedView from './components/EventDetailedView';
 
 function App() {
   const [favorites, setFavorites] = useState([]);
 
+  const history = useHistory()
   const image = window.innerWidth >= 650 ? img_desktop : img_mobile;
+
+  function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
 
   const setAsFavorite = (e, event) => {
     e.stopPropagation()
@@ -27,25 +34,32 @@ function App() {
     setFavorites(newFavorites)
   }
 
+  const navigateToEvent = (id) => {
+    history.push('/events/' + id)
+  }
+
   return (
-    <Router>
       <MuiThemeProvider theme={theme}>
       <div className="gridContainer" style={{ backgroundImage:`url("${image}")`, backgroundRepeat: 'no-repeat', color: 'white', backgroundSize: 'cover' }}>
         <div style={{ backgroundColor: 'rgba(100, 52, 128, 0.49)', height: '100vh' }}>
           <Header favorites={favorites} removeFavorite={removeFavorite}/>
           <Switch>
-            <Route exact path="/">
-              <EventsContainer favorites={favorites} setAsFavorite={setAsFavorite}/>
+            <Route exact path={["/", "/?categories:"]}>
+              <EventsContainer 
+                categoryFilters={useQuery().get("categories")} 
+                history={history} 
+                favorites={favorites} 
+                setAsFavorite={setAsFavorite} 
+                onEventClick={navigateToEvent}/>
             </Route>
-            <Route path="/?categories:">
-              <EventsContainer favorites={favorites} setAsFavorite={setAsFavorite}/>
+            <Route path="/events/:id">
+              <EventDetailedView/>
             </Route>
           </Switch>
         </div>
         <div className="footer"><p>© {new Date().getFullYear()} Paperstr, Inc.</p></div>
       </div>
       </MuiThemeProvider>
-    </Router>
   );
 }
 
