3808863a2963f50c45d245a5f2a243339e071e41
Susanna <sfalan@utu.fi>
2020-07-26 00:02:41 +0300

    handle case if event is undefined

--- src/functions/getCategories.js
+++ src/functions/getCategories.js
@@ -1,4 +1,6 @@
 export const getCategories = (event) => {
+    if (!event) return "Categories not found"
+
     const keys = Object.keys(event.category)
     const categories = keys.map(key => {
       return event.category[key].title
--- src/functions/getName.js
+++ src/functions/getName.js
@@ -1,4 +1,6 @@
 export const getName = (event) => {
+    if (!event) return "Name not found"
+
     const keys = Object.keys(event.name)
     if (keys.includes('en')) {
       return event.name.en
