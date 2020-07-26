ba93f2bc5ea3ba76aa097133682dee6c0f5bd35a
Susanna <sfalan@utu.fi>
2020-07-26 22:46:32 +0300

    remove seconds from times shown

--- src/functions/formatTime.js
+++ src/functions/formatTime.js
@@ -3,15 +3,12 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 export const formatTime = (startTime) => {
     const date = new Date(startTime * 1000)
     const minute = date.getMinutes()
-    const second = date.getSeconds()
     const minuteFormatted = minute < 10 ? "0" + minute : minute
-    const secondFormatted = second < 10 ? "0" + second : second
     
     return weekdays[date.getDay()] + ' ' 
             + date.getDate() + '.' 
             + (date.getMonth() + 1) + '.' 
             + date.getFullYear() + ' ' 
             + date.getHours() + ':' 
-            + minuteFormatted + ':' 
-            + secondFormatted
+            + minuteFormatted
   }
